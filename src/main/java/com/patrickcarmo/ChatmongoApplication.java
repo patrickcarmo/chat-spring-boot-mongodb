aabd2d9efae30ef4d8c4203ac9cf5bb15892f84e
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-02-19 20:38:16 -0300

    projeto criado

---
+++ src/main/java/com/patrickcarmo/ChatmongoApplication.java
@@ -0,0 +1,12 @@
+package com.patrickcarmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatmongoApplication.class, args);
+	}
+}
