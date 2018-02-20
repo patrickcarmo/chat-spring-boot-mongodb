905f33c07bdffb4b9dbebe0ff9fb7ab75ce39b75
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-02-20 19:19:01 -0300

    endepoint para retornar os posts de um usuario

--- src/main/java/com/patrickcarmo/chatmongo/resources/UserResource.java
+++ src/main/java/com/patrickcarmo/chatmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.patrickcarmo.chatmongo.domain.Post;
 import com.patrickcarmo.chatmongo.domain.User;
 import com.patrickcarmo.chatmongo.dto.UserDTO;
 import com.patrickcarmo.chatmongo.services.UserService;
@@ -57,6 +58,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
