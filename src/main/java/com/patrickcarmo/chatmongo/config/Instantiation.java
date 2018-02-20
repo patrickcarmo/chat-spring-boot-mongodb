119dea24fbfa695f90bd0bb84a6315151690debb
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-02-20 19:48:57 -0300

    acrescentando comentario aos posts

--- src/main/java/com/patrickcarmo/chatmongo/config/Instantiation.java
+++ src/main/java/com/patrickcarmo/chatmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.patrickcarmo.chatmongo.domain.Post;
 import com.patrickcarmo.chatmongo.domain.User;
 import com.patrickcarmo.chatmongo.dto.AuthorDTO;
+import com.patrickcarmo.chatmongo.dto.CommentDTO;
 import com.patrickcarmo.chatmongo.repository.PostRepository;
 import com.patrickcarmo.chatmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("20/02/2018"), "Partiu cama", "Vou ir dormir, boa noite!", new AuthorDTO(alex));
 		Post post2 = new Post(null, sdf.parse("21/02/2018"), "Bom dia", "Hoje eu acordei para sorrir mostrar os dentes!", new AuthorDTO(alex));
+		
+		CommentDTO comment1 = new CommentDTO("Boa noite!", sdf.parse("20/02/2018"), new AuthorDTO(maria));
+		CommentDTO comment2 = new CommentDTO("Essa música é top!", sdf.parse("21/02/2018"), new AuthorDTO(maria));
+		CommentDTO comment3 = new CommentDTO("Topp.. não se faz mais música como antigamente! \\m/", sdf.parse("21/02/2018"), new AuthorDTO(bob));
+		
+		post1.getComments().addAll(Arrays.asList(comment1));
+		post2.getComments().addAll(Arrays.asList(comment2, comment3));
 		
 		postRepository.save(Arrays.asList(post1, post2));
 		
