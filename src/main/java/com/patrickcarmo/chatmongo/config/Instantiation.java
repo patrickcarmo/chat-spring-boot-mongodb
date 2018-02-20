ea040466db6e598f6171de8ec6bc3bd20f48378e
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-02-19 23:29:43 -0300

    criando entity Post com User aninhado

--- src/main/java/com/patrickcarmo/chatmongo/config/Instantiation.java
+++ src/main/java/com/patrickcarmo/chatmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.patrickcarmo.chatmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.patrickcarmo.chatmongo.domain.Post;
 import com.patrickcarmo.chatmongo.domain.User;
+import com.patrickcarmo.chatmongo.repository.PostRepository;
 import com.patrickcarmo.chatmongo.repository.UserRepository;
 
 @Configuration
@@ -15,15 +19,27 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository; 
 	
+	@Autowired
+	private PostRepository postRepository; 
+	
 	@Override
 	public void run(String... arg0) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		Post post1 = new Post(null, sdf.parse("20/02/2018"), "Partiu cama", "Vou ir dormir, boa noite!", alex);
+		Post post2 = new Post(null, sdf.parse("21/02/2018"), "Bom dia", "Hoje eu acordei para sorrir mostrar os dentes!", alex);
+		
 		userRepository.save(Arrays.asList(maria, alex, bob));
+		postRepository.save(Arrays.asList(post1, post2));
 	}
 
 }
