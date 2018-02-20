b8f4787d503c47917f0c4c6b45243e73089a293c
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-02-20 19:02:34 -0300

    projecao dos dados do author com DTO

--- src/main/java/com/patrickcarmo/chatmongo/config/Instantiation.java
+++ src/main/java/com/patrickcarmo/chatmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.patrickcarmo.chatmongo.domain.Post;
 import com.patrickcarmo.chatmongo.domain.User;
+import com.patrickcarmo.chatmongo.dto.AuthorDTO;
 import com.patrickcarmo.chatmongo.repository.PostRepository;
 import com.patrickcarmo.chatmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("20/02/2018"), "Partiu cama", "Vou ir dormir, boa noite!", alex);
-		Post post2 = new Post(null, sdf.parse("21/02/2018"), "Bom dia", "Hoje eu acordei para sorrir mostrar os dentes!", alex);
-		
 		userRepository.save(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("20/02/2018"), "Partiu cama", "Vou ir dormir, boa noite!", new AuthorDTO(alex));
+		Post post2 = new Post(null, sdf.parse("21/02/2018"), "Bom dia", "Hoje eu acordei para sorrir mostrar os dentes!", new AuthorDTO(alex));
+		
 		postRepository.save(Arrays.asList(post1, post2));
 	}
 
