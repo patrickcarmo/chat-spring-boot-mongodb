99856e8602cc980d7ffcd5613519fd34b6aa5e4a
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-02-19 22:12:44 -0300

    obtem usuario por id e trata erros

--- src/main/java/com/patrickcarmo/chatmongo/services/UserService.java
+++ src/main/java/com/patrickcarmo/chatmongo/services/UserService.java
@@ -7,15 +7,24 @@ import org.springframework.stereotype.Service;
 
 import com.patrickcarmo.chatmongo.domain.User;
 import com.patrickcarmo.chatmongo.repository.UserRepository;
+import com.patrickcarmo.chatmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	private UserRepository repo;
+	private UserRepository repository;
 	
 	public List<User> findAll(){
-		return repo.findAll();
+		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repository.findOne(id);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado!");
+		}
+		return user;
 	}
 	
 }
